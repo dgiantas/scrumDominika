845e6ca7608b1264eb53657cc0c9180144c423da
Dominika G <DomiGiantas@outlook.com>
2020-05-03 12:33:55 +0300

    New commit

--- src/java/database/QuestionPossibleAnswers.java
+++ src/java/database/QuestionPossibleAnswers.java
@@ -19,31 +19,24 @@ import java.util.logging.Logger;
 public class QuestionPossibleAnswers {
 
         
-        private static ResultSet getQuestionById(int id) {
+        public static ResultSet getQuestionById(int id) {
         return Database.getResults(
-                "SELECT \n"+ "    text_quest, text_ans\n"+ "FROM\n"+ "    questions,\n"+ "    answers\n" + "WHERE\n" + "    questions.id = answers.question_id\n" + "        AND questions.id = " + id + ";");
+                "SELECT \n"+ "text_quest, text_ans\n"+ "FROM\n"+ "questions,\n"+ "answers\n" + "WHERE\n" + "questions.id = answers.question_id\n" + "AND questions.id = " + id + ";");
     }
         
 
        public static void getQuestionWithPossibleAnswers() {
         ResultSet rs = getQuestionById(1);
+        
         String question="question1";
         String answer;
+       
         List<String> answers = new ArrayList();
-        try {
-            if(rs.next()){
-                question = rs.getString(1);
-                answer = rs.getString(2);
-                answers.add(answer);
-            }
-            while (rs.next()) {
-                answer = rs.getString(2);
-                answers.add(answer);
-            }
-        } catch (SQLException ex) {
-            Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        System.out.println(question);
+        for(String answer : answers) {
+                 System.out.println(question);
         System.out.println(answers);
+         
+       
     }
-    }
+       }
+    
